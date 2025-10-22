7348ddecb4943130b957f81794144c97ee5e2910
selukiler <jfc0007@alu.medac.es>
2025-10-22 12:53:23 +0200

    Completa la vista dinámica de tareas (UD2)

--- public/desafios.php
+++ public/desafios.php
@@ -1,6 +1,16 @@
 <?php
 
 //Desafío 1: La Sucesión de Fibonacci
-for ($i=0; $i<=20; $i++){
-    echo $i + ($i-1)+($i-2).'<br>';
+$a=0;
+$b=1;
+echo $a.'<br>';
+echo $b.'<br>';
+for ($i=0; $i<18; $i++){
+    $prox=$a+$b;
+    echo $prox.'<br>';
+    $a=$b;
+    $b=$prox;
 }
+
+//Desafío 2: Comprobador de Números Primos
+
